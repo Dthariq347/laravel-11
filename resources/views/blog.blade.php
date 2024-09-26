b25ec97010afc663f85d51fe0c2abbc025287f97
Dzaky Abiyyu Thariq <65454556+Dthariq347@users.noreply.github.com>
2024-09-26 22:14:24 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>blog</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">blog</a>
+    <a href="/about">about</a>
+    <a href="/contact">contact</a>
+
+    <h1>HALAMAN BLOG</h1>
+
+    <article>
+        <h3>article 1</h3>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, non. Recusandae dolores Zaliquid nesciunt
+        quae
+        cum corporis, suscipit doloribus vitae delectus? Veritatis ab fugit deserunt repellendus et nesciunt ea
+        consectetur?
+    </article>
+    <article>
+        <h3>article 2</h3>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, non. Recusandae dolores Zaliquid nesciunt
+        quae
+        cum corporis, suscipit doloribus vitae delectus? Veritatis ab fugit deserunt repellendus et nesciunt ea
+        consectetur?
+    </article>
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>contact</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">blog</a>
+    <a href="/about">about</a>
+    <a href="/contact">contact</a>
+
+    <h1>halaman contact</h1>
+    <ul>
+        <li>
+            instagram: <a href="https://instagram.com/dthariq347"></a>
+        </li>
+        <li>
+            twitter: <a href="#"></a>
+        </li>
+    </ul>
+</body>
+
+</html>
